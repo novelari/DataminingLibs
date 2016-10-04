d15ecfa4e7bd351c03f5d40626b73b8b4c38dd03
mahmoudelgamal <mahmoudf.elgamal@gmail.com>
2016-10-04 10:42:37 +0200

    Add fitData method to juliacall for testing

--- lib_juliacall/julia_to_c.cc
+++ lib_juliacall/julia_to_c.cc
@@ -1,5 +1,21 @@
+#pragma once
+#include "test_resources.h"
 
-int getDouble (int x){
-	return x*2;
+
+bool fitData (){
+	auto &algorithms_face = AlgorithmsLib::GetInstance();
+	auto &ensembles_face = EnsemblesLib::GetInstance();
+
+	sp<lib_models::MlModel> model_flt;
+	sp<lib_models::MlModel> model_dbl;
+	auto gpurf_flt = ensembles_face.CreateGpuRfAlgorithm<float>();
+	auto gpurf_dbl = ensembles_face.CreateGpuRfAlgorithm<double>();
+
+	auto params = ensembles_face.CreateGpuRfParamPack();
+	params->Set(EnsemblesLib::kNrTrees, 100);
+	params->Set(EnsemblesLib::kAlgoType, AlgorithmsLib::kClassification);
+	model_flt =	gpurf_flt->Fit(lib_ensembles::data_fit_raw_flt, params);
+
+	return true;
 
 }
